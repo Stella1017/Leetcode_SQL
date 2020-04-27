a5e11a985ffb4cd38343074bf80fb5017bc9c4b6
Stella1017 <list.stella@gmail.com>
2020-04-27 09:38:15 -0700

    Update 1045_medium.sql

--- 1045_medium.sql
+++ 1045_medium.sql
@@ -1,22 +1,29 @@
-# 1045. Customers Who Bought All Products
-# Table: Customer
-# +-------------+---------+
-# | Column Name | Type    |
-# +-------------+---------+
-# | customer_id | int     |
-# | product_key | int     |
-# +-------------+---------+
-# product_key is a foreign key to Product table.
+-- 1045. Customers Who Bought All Products
+/*
+Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+product_key is a foreign key to Product table.
 
-# Table: Product
-# +-------------+---------+
-# | Column Name | Type    |
-# +-------------+---------+
-# | product_key | int     |
-# +-------------+---------+
-# product_key is the primary key column for this table.
+Table: Product
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key column for this table.
  
-#  Write an SQL query for a report that provides the customer ids from the Customer table that bought all the products in the Product table.
+Write an SQL query for a report that provides the customer ids from the Customer table that bought all the products in the Product table.
+
+Result table:
++-------------+
+| customer_id |
++-------------+
+*/
 
 SELECT customer_id
 FROM Customer
