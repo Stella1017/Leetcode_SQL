f4063d07f4a1f786f8eb54dd042fe4dc401ed0d7
Stella1017 <list.stella@gmail.com>
2020-04-27 09:36:42 -0700

    Update 1070_medium.sql

--- 1070_medium.sql
+++ 1070_medium.sql
@@ -1,38 +1,37 @@
-# 1070. Product Sales Analysis III
+-- 1070. Product Sales Analysis III
+/*
+Table: Sales
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+sale_id is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
 
-# Table: Sales
-
-# +-------------+-------+
-# | Column Name | Type  |
-# +-------------+-------+
-# | sale_id     | int   |
-# | product_id  | int   |
-# | year        | int   |
-# | quantity    | int   |
-# | price       | int   |
-# +-------------+-------+
-# sale_id is the primary key of this table.
-# product_id is a foreign key to Product table.
-# Note that the price is per unit.
-# Table: Product
-
-# +--------------+---------+
-# | Column Name  | Type    |
-# +--------------+---------+
-# | product_id   | int     |
-# | product_name | varchar |
-# +--------------+---------+
-# product_id is the primary key of this table.
+Table: Product
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
  
+Write an SQL query that selects the product id, year, quantity, and price for the first year of every product sold.
 
-# Write an SQL query that selects the product id, year, quantity, and price for the first year of every product sold.
+Result table:
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+*/
 
-# Result table:
-# +------------+------------+----------+-------+
-# | product_id | first_year | quantity | price |
-# +------------+------------+----------+-------+ 
-
-# MS SQL with Window Function
+-- MS SQL with Window Function
 SELECT 
     product_id, 
     first_year,
@@ -48,7 +47,7 @@ FROM
     FROM Sales) t
 WHERE YearRank = 1;
 
-# MySQL (faster)
+-- MySQL (faster)
 SELECT 
     S.product_id,
     tmp.first_year,
@@ -62,6 +61,6 @@ LEFT JOIN Sales S
     ON S.product_id = tmp.product_id 
     AND S.year = tmp.first_year;
     
-# There could be multiple sales records for one product in the first year, the question didn't specify how to deal with it, 
-# and the "expected answer" just kept all the records
+/* There could be multiple sales records for one product in the first year, the question didn't specify how to deal with it, 
+and the "expected answer" just kept all the records*/
 
