7ae383cd63709c299c875cb19a7a6f60ef0077df
Stella1017 <list.stella@gmail.com>
2020-05-22 10:34:24 -0700

    Create 1193_medium.sql

---
+++ 1193_medium.sql
@@ -0,0 +1,48 @@
+-- 1193. Monthly Transactions I
+
+/*
+
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+
+Write an SQL query to find for each month and country, the number of transactions and 
+their total amount, the number of approved transactions and their total amount.
+
+The query result format is in the following example:
+
+Result table:
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+*/
+
+SELECT 
+    month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM
+    (
+    SELECT 
+        country, 
+        state, 
+        amount,
+        LEFT(CAST(trans_date AS varchar), 7) AS month -- only keep year and month information
+    FROM Transactions    
+    ) tmp
+GROUP BY month, country
